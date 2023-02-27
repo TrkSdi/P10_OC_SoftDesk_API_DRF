63531ea972001bd1fa8527dfa237e7b6d0d728b0
Tarik Sadkhi <tarik.sadkhi@gmail.com>
2023-02-27 16:23:00 +0100

    fixing signup by adding username fields (not required)

--- src/auth/models.py
+++ src/auth/models.py
@@ -8,6 +8,9 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField()
+    username = models.CharField(max_length=30, null=True, blank=True, unique=True)
     
     def __str__(self):
+        if self.first_name == "":
+            return self.username
         return self.first_name
